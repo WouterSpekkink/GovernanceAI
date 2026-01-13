83e6933fce2e6b1a5cb7aff6839afe906aa04a74
Wouter Spekkink <wahsp@tutanota.com>
2026-01-13 09:48:23 +0100

    Added text export

--- app.py
+++ app.py
@@ -75,14 +75,16 @@ with st.sidebar:
         st.rerun()
 
     st.markdown("### Export")
+    has_messages = bool(st.session_state.get("messages"))
+
     st.download_button(
         label="Download conversation (TXT)",
-        data=chat_as_text(st.session_state.messages),
+        data=chat_as_text(st.session_state.get("messages", [])),
         file_name=f"bernaid_chat_{st.session_state.session_started_at}.txt",
         mime="text/plain",
-        disabled=(len(st.session_state.messages) == 0),
-    )
-       
+        disabled=not has_messages,
+        )
+
 # ----------------------------
 # Render chat history
 # ----------------------------
